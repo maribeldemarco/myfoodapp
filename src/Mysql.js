72fca090d0318e797cdbb99a86f3570dc47f313b
Linda M De Marco <maribeldemarco@gmail.com>
2023-04-17 17:25:22 -0300

    subo base de datos

---
+++ src/Mysql.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mysql = require("mysql2");
+const cors = require ("cors");
+
+
+const db =mysql.createPool(
+    {   host: "localhost",
+        user: "root",
+        password: "01478963.",
+        database: "colaboradores_refugio"
+        }
+);
+
+app.use(cors());
+
+app.get('/myapp/prueba', (req, res)=>{
+    
+ 
+    
+    console.log('Prueba');
+});
+
+
+app.get('/myapp/prueba', (req, res)=>{
+    
+ const q= "SELECT Nombre, Apellido FROM datos_colaboradores"
+ db.query(q, (err,data)=> {
+   
+   if(err){return res.json(err);}
+   else {
+    console.log(data)}
+ })    
+
+});
+
+
+
+
+
+
+app.get('/myapp/test/', (req, res)=>{
+    const q= "SELECT * FROM datos_colaboradores"
+ db.query(q, (err,data)=> {
+  
+   if(err) return res.json(err)
+   return res.send(data);
+ })    
+    
+});
+
+app.listen(5000, () => console.log('Application is ok'));
